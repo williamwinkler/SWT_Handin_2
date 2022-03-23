f9ad1f4345a3655eab3f7012526901605f365108
William Winkler <79922275+williamwinkler@users.noreply.github.com>
2022-03-23 15:32:20 +0100

    ILogFile added

---
+++ SWTF22_21_Handin2_ChargingStation.Lib/ILogFile.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWTF22_21_Handin2_ChargingStation.Lib
+{
+    public interface ILogFile
+    {
+        public void WriteToLog(string message, DateTime timeStamp);
+    }
+}
