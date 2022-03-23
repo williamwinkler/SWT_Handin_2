7ef739323748d05fa9332de65e3225c208ddef19
Jacob Mørk Brejnbjerg <au661789@uni.au.dk>
2022-03-23 15:55:22 +0100

    Added Door and renamed program function calls to better names

---
+++ SWTF22_21_Handin2_ChargingStation.Lib/Door.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWTF22_21_Handin2_ChargingStation.Lib
+{
+    public class Door : IDoor
+    {
+        public void CloseDoor()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OpenDoor()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SWTF22_21_Handin2_ChargingStation.Lib/IDoor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWTF22_21_Handin2_ChargingStation.Lib
+{
+    public interface IDoor
+    {
+        public void OpenDoor();
+
+        public void CloseDoor();
+    }
+}
