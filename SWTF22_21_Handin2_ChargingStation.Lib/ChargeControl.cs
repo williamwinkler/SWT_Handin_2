d151769e08805e7519643fc2cc1a2b89a9acefaa
Jacob Mørk Brejnbjerg <au661789@uni.au.dk>
2022-03-23 15:49:51 +0100

    Implemented UsbCharger

--- SWTF22_21_Handin2_ChargingStation.Lib/ChargeControl.cs
+++ SWTF22_21_Handin2_ChargingStation.Lib/ChargeControl.cs
@@ -3,24 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UsbSimulator;
 
 namespace SWTF22_21_Handin2_ChargingStation.Lib
 {
     public class ChargeControl : IChargeControl
     {
+        private readonly IUsbCharger _usbCharger;
+
+        public ChargeControl(IUsbCharger usbCharger)
+        {
+            _usbCharger = usbCharger;
+        }
+
         public bool IsConnected()
         {
-            throw new NotImplementedException();
+            return _usbCharger.Connected;
         }
 
         public void StartCharging()
         {
-            throw new NotImplementedException();
+            _usbCharger.StartCharge();
         }
 
         public void StopCharging()
         {
-            throw new NotImplementedException();
+            _usbCharger.StartCharge();
         }
     }
 }
