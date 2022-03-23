86d0258505755341853037d6f7fd9f81e6b5578c
Jacob Mørk Brejnbjerg <au661789@uni.au.dk>
2022-03-23 16:15:07 +0100

    Added door event

--- SWTF22_21_Handin2_ChargingStation.Lib/Door.cs
+++ SWTF22_21_Handin2_ChargingStation.Lib/Door.cs
@@ -1,28 +1,39 @@
 ﻿namespace SWTF22_21_Handin2_ChargingStation.Lib
 {
-    public class Door : IDoor
+    public class Door : EventArgs, IDoor
     {
+        public event EventHandler<Door> DoorMoveEvent;
+
         public bool Closed { get; set; }
         public bool Locked { get; set; }
 
         public void CloseDoor()
         {
             Closed = true;
+            OnDoorStateChangedEvent(this);
         }
 
         public void OpenDoor()
         {
             Closed = false;
+            OnDoorStateChangedEvent(this);
         }
 
         public void UnlockDoor()
         {
             Locked = false;
+            OnDoorStateChangedEvent(this);
         }
 
         public void LockDoor()
         {
             Locked = true;
+            OnDoorStateChangedEvent(this);
+        }
+
+        private void OnDoorStateChangedEvent(Door e)
+        {
+            DoorMoveEvent?.Invoke(this, e);
         }
     }
 }
