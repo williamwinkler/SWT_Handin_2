16dc110a6da58c70737fce02505875e1efec9708
AlExIsNoTaVaIlAbL <98851343+AlExIsNoTaVaIlAbL@users.noreply.github.com>
2022-03-23 18:12:54 +0100

    Changed program from .5 to .6 with added text

--- SWTF22_21_Handin2_ChargingStation.Console/Program.cs
+++ SWTF22_21_Handin2_ChargingStation.Console/Program.cs
@@ -1,41 +1,51 @@
 ﻿using SWTF22_21_Handin2_ChargingStation.Lib;
 
-bool finish = false;
-
+IUsbCharger usbCharger = new UsbChargerSimulator();
+IChargeControl chargeControl = new ChargeControl(usbCharger);
 IDoor door = new Door();
+IDisplay display = new Display();
 IRFIDReader rfidReader = new RFIDReader();
+ILogFile logFile = new LogFile();
 
+new StationControl(chargeControl, door, display, usbCharger, rfidReader, logFile);
 
-
+bool finish = false;
 do
 {
     string input;
-    System.Console.WriteLine("Indtast E, O, C, R: ");
+    System.Console.WriteLine("Indtast (E)nd, (O)pen, (C)lose, (R)ead Card, Connect (P)hone: ");
     input = Console.ReadLine();
     if (string.IsNullOrEmpty(input)) continue;
 
-    switch (input[0])
+    switch (input[0].ToString().ToUpper())
     {
-        case 'E':
-            finish = true;
+        case "E":
+            System.Environment.Exit(1);
             break;
 
-        case 'O':
+        case "O":
             door.OpenDoor();
             break;
 
-        case 'C':
+        case "C":
             door.CloseDoor();
             break;
 
-        case 'R':
-            System.Console.WriteLine("Indtast RFID id: ");
-            string idString = System.Console.ReadLine();
-
-            int id = Convert.ToInt32(idString);
-            rfidReader.EnterCardId(id); // updated method name
+        case "R":
+            Console.WriteLine("Indtast RFID kode");
+            rfidReader.EnterCardId(int.Parse(Console.ReadLine()));
             break;
 
+        case "P":
+            if (door.Closed)
+                Console.WriteLine("Cant connect phone when door is closed");
+            else
+            {
+                usbCharger.Connected = true;
+                Console.WriteLine("Phone connected");
+            }
+
+            break;
         default:
             break;
     }
