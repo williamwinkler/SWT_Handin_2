836e4ded40b9583b7a37ccc477763910a67d17fc
William Winkler <79922275+williamwinkler@users.noreply.github.com>
2022-03-23 14:37:18 +0100

    Test added

---
+++ SWTF22_21_Handin2_ChargingStation/SWTF22_21_Handin2_ChargingStation.Test.Unit/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace SWTF22_21_Handin2_ChargingStation.Test.Unit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
