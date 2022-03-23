6b25352e4632bb56900cb266ceb925ea3d306e75
William Winkler <79922275+williamwinkler@users.noreply.github.com>
2022-03-23 15:59:52 +0100

    tttt

---
+++ SWTF22_21_Handin2_ChargingStation.Lib/RFIDReader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWTF22_21_Handin2_ChargingStation.Lib
+{
+    public class RFIDReader
+    {
+    }
+}
