666f5a25dce36c02d0e3bc7ea61d2592b26fed14
William Winkler <79922275+williamwinkler@users.noreply.github.com>
2022-03-23 15:30:16 +0100

    LogEntry added

---
+++ SWTF22_21_Handin2_ChargingStation.Lib/LogEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWTF22_21_Handin2_ChargingStation.Lib
+{
+    public class LogEntry
+    {
+        public string LogMessage { get; set; }
+        public DateTime TimeStamp { get; set; }
+    }
+}
