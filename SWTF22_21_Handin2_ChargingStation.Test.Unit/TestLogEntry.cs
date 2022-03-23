934a26c242a409eced824400772931903c2aabea
William Winkler <williambwinkler@gmail.com>
2022-03-23 23:17:41 +0100

    add test for EntryLog

---
+++ SWTF22_21_Handin2_ChargingStation.Test.Unit/TestLogEntry.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using SWTF22_21_Handin2_ChargingStation.Lib;
+using System;
+
+namespace SWTF22_21_Handin2_ChargingStation.Test.Unit
+{
+    public class TestLogEntry
+    {
+        private LogEntry _uut;
+      
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new LogEntry();
+        }
+
+        [TestCase("Testing LogMessage", "2022-03-23")]
+        public void LogEntry_AddLogMessageAndDateTime_ReturnSame(string log, DateTime d)
+        {
+            _uut.LogMessage = log;
+            _uut.TimeStamp = d;
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(_uut.LogMessage, Is.EqualTo(log));
+                Assert.That(_uut.TimeStamp, Is.EqualTo(d));
+            });
+        }
+    }
+}
