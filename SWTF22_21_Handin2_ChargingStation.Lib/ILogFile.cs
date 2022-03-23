e9344f33b8d00104624c7256d19bdc3cd5a20ace
William Winkler <79922275+williamwinkler@users.noreply.github.com>
2022-03-23 16:43:22 +0100

    added ref

--- SWTF22_21_Handin2_ChargingStation.Lib/ILogFile.cs
+++ SWTF22_21_Handin2_ChargingStation.Lib/ILogFile.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SWTF22_21_Handin2_ChargingStation.Lib
+﻿namespace SWTF22_21_Handin2_ChargingStation.Lib
 {
     public interface ILogFile
     {
--- SWTF22_21_Handin2_ChargingStation.Lib/StationControl.cs
+++ SWTF22_21_Handin2_ChargingStation.Lib/StationControl.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SWTF22_21_Handin2_ChargingStation.Lib
+﻿namespace SWTF22_21_Handin2_ChargingStation.Lib
 {
     public class StationControl
     {
