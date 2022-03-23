956f75734c953a0f26f60e9d4259108be51c8bed
William Winkler <79922275+williamwinkler@users.noreply.github.com>
2022-03-23 16:12:29 +0100

    næsten færdig med RFIDReader

--- SWTF22_21_Handin2_ChargingStation.Lib/RFIDReader.cs
+++ SWTF22_21_Handin2_ChargingStation.Lib/RFIDReader.cs
@@ -6,14 +6,47 @@ using System.Threading.Tasks;
 
 namespace SWTF22_21_Handin2_ChargingStation.Lib
 {
-    public class RFIDReader
+    public class RFIDReader : IRFIDReader
     {
-        
+        public void EnterCardId(int id)
+        {
+            this.cardID = id;
+        }
+        protected virtual void OnScanEvent(ScanEventArgs eventArgs)
+        {
+            ScanEvent?.Invoke(this, eventArgs);
+        }
+
+        private int cardID;
+        public bool Error { get; set; }
+        public int CardID
+        {
+            get { return cardID; }
+
+            set {
+                    if (0 <= value)
+                    {
+                        Error = false;
+                        OnScanEvent(new ScanEventArgs { ID = value });
+                        cardID = value;
+                    }
+                    else
+                    {
+                        Error = true;
+                        Console.WriteLine("CardID not positive! Please enter postive CardID");
+                    }
+                }
+
+        }
+
+
+
+        public event EventHandler<ScanEventArgs> ScanEvent;
     }
     
     public class ScanEventArgs : EventArgs
     {
-        public int ID { get; set; };
+        public int ID { get; set; }
     }
 }
 
