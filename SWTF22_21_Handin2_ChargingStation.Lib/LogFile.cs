2606c6eff2ef4af16d75fb9bcd48a9eab01e322b
William Winkler <79922275+williamwinkler@users.noreply.github.com>
2022-03-23 15:37:06 +0100

    Logfile implementering added

---
+++ SWTF22_21_Handin2_ChargingStation.Lib/LogFile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWTF22_21_Handin2_ChargingStation.Lib
+{
+    public class LogFile : ILogFile
+    {
+        public List<LogEntry> Logs { get; set; }
+
+        public LogFile()
+        {
+            Logs = new List<LogEntry>();
+        }
+
+        public void WriteToLog(string message, DateTime timeStamp)
+        {
+            Logs.Add(new LogEntry { LogMessage = message, TimeStamp = timeStamp });
+        }
+    }
+}
