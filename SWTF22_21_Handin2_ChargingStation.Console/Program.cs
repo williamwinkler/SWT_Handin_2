ca2ad39348bb12b3b8aea68cd8fb92c2e6f19ffc
William Winkler <79922275+williamwinkler@users.noreply.github.com>
2022-03-23 15:48:36 +0100

    not working...

--- SWTF22_21_Handin2_ChargingStation.Console/Program.cs
+++ SWTF22_21_Handin2_ChargingStation.Console/Program.cs
@@ -1,35 +1,48 @@
-﻿bool finish = false;
-do
+﻿using SWTF22_21_Handin2_ChargingStation.Lib;
+
+static void Main()
 {
-    string input;
-    System.Console.WriteLine("Indtast E, O, C, R: ");
-    input = Console.ReadLine();
-    if (string.IsNullOrEmpty(input)) continue;
+    var Logs = new LogFile();
 
-    switch (input[0])
-    {
-        case 'E':
-            finish = true;
-            break;
+    Logs.WriteToLog("Test", new DateTime());
 
-        case 'O':
-            door.OnDoorOpen();
-            break;
+    Console.WriteLine(Logs.ToString());
+}
 
-        case 'C':
-            door.OnDoorClose();
-            break;
 
-        case 'R':
-            System.Console.WriteLine("Indtast RFID id: ");
-            string idString = System.Console.ReadLine();
 
-            int id = Convert.ToInt32(idString);
-            rfidReader.OnRfidRead(id);
-            break;
+//bool finish = false;
+//do
+//{
+//    string input;
+//    System.Console.WriteLine("Indtast E, O, C, R: ");
+//    input = Console.ReadLine();
+//    if (string.IsNullOrEmpty(input)) continue;
 
-        default:
-            break;
-    }
+//    switch (input[0])
+//    {
+//        case 'E':
+//            finish = true;
+//            break;
 
-} while (!finish);
+//        case 'O':
+//            door.OnDoorOpen();
+//            break;
+
+//        case 'C':
+//            door.OnDoorClose();
+//            break;
+
+//        case 'R':
+//            System.Console.WriteLine("Indtast RFID id: ");
+//            string idString = System.Console.ReadLine();
+
+//            int id = Convert.ToInt32(idString);
+//            rfidReader.OnRfidRead(id);
+//            break;
+
+//        default:
+//            break;
+//    }
+
+//} while (!finish);
