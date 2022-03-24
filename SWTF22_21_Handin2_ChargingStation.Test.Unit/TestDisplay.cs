ba7a1e09043c85699411e9465f5d6f3624d092d4
Jacob Mørk Brejnbjerg <au661789@uni.au.dk>
2022-03-24 10:39:27 +0100

    Finished UnitTest for TestDisplay so it understands Console.Writeline

--- SWTF22_21_Handin2_ChargingStation.Test.Unit/TestDisplay.cs
+++ SWTF22_21_Handin2_ChargingStation.Test.Unit/TestDisplay.cs
@@ -1,16 +1,36 @@
-﻿using System;
+﻿using NUnit.Framework;
+using SWTF22_21_Handin2_ChargingStation.Lib;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SWTF22_21_Handin2_ChargingStation.Test.Unit
 {
+    [TestFixture]
     public class TestDisplay
     {
+        private readonly IDisplay _display;
+
         public TestDisplay()
         {
-             
+            _display = new Display();
+        }
+
+        [TestCase("This is a very long message This is a very long message This is a very long message This is a very long message")]
+        [TestCase("This is a normal message")]
+        [TestCase("")]
+        public void DisplayMessage_ConsolePrint_CorrectMessage(string message)
+        {
+            var sw = new StringWriter();
+            Console.SetOut(sw);
+
+            _display.DisplayMessage(message);
+         
+            // \r\n is because Console.WriteLine creates a new line
+            Assert.AreEqual($"Display: {message}\r\n", sw.ToString());
         }
     }
 }
