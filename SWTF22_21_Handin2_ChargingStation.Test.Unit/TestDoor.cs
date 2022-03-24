e128233d4d2b51042b7dcfdf19a6f911e8d624d2
Jacob Mørk Brejnbjerg <au661789@uni.au.dk>
2022-03-24 10:57:26 +0100

    Added TestDoor tests

--- SWTF22_21_Handin2_ChargingStation.Test.Unit/TestDoor.cs
+++ SWTF22_21_Handin2_ChargingStation.Test.Unit/TestDoor.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NUnit.Framework;
+using SWTF22_21_Handin2_ChargingStation.Lib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,79 @@ using System.Threading.Tasks;
 
 namespace SWTF22_21_Handin2_ChargingStation.Test.Unit
 {
+    [TestFixture]
     public class TestDoor
     {
+        private readonly IDoor _door;
+
+        public TestDoor()
+        {
+            _door = new Door();
+        }
+
+        [Test]
+        public void DoorClosed_CloseDoor_DoorStillClosed()
+        {
+            _door.Closed = true;
+            _door.CloseDoor();
+            Assert.IsTrue(_door.Closed);
+        }
+
+        [Test]
+        public void DoorOpen_OpenDoor_DoorStillOpen()
+        {
+            _door.Closed = false;
+            _door.OpenDoor();
+            Assert.IsFalse(_door.Closed);
+        }
+
+        [Test]
+        public void DoorOpen_CloseDoor_DoorIsClosed()
+        {
+            _door.Closed = false;
+            _door.CloseDoor();
+            Assert.IsTrue(_door.Closed);
+        }
+        
+        [Test]
+        public void DoorClosed_OpenDoor_DoorIsOpen()
+        {
+            _door.Closed = true;
+            _door.OpenDoor();
+            Assert.IsFalse(_door.Closed);
+        }
+
+        [Test]
+        public void DoorLocked_CloseAndUnlockDoor_DoorIsUnlocked()
+        {
+            _door.Locked = true;
+            _door.CloseDoor();
+            _door.UnlockDoor();
+            Assert.IsFalse(_door.Locked);
+        }
+
+        [Test]
+        public void DoorUnlocked_LockDoor_DoorIsLocked()
+        {
+            _door.Locked = false;
+            _door.LockDoor();
+            Assert.IsTrue(_door.Locked);
+        }
+
+        [Test]
+        public void DoorLocked_OpenDoor_DoorIsLocked()
+        {
+            _door.Locked = true;
+            _door.OpenDoor();
+            Assert.IsTrue(_door.Locked);
+        }
+
+        [Test]
+        public void DoorUnlocked_CloseDoor_DoorIsUnlocked()
+        {
+            _door.Locked = false;
+            _door.CloseDoor();
+            Assert.IsFalse(_door.Locked);
+        }
     }
 }
